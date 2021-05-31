2f1bd257c9b9572a6c81c86616db5c1a2d89a4c0
Yamin Alam Sarker <yaminalam27@gmail.com>
2021-05-31 11:25:53 +0600

    modern login starter kit

---
+++ webpack.mix.js
@@ -0,0 +1,27 @@
+const mix = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix.js('resources/js/app.js', 'public/js')
+    .sass('resources/sass/app.scss', 'public/css',  {
+        sassOptions: {
+            includePaths: ['node_modules'],
+        },
+    });
+mix.browserSync({
+    proxy: 'localhost:8000',
+    notify: true
+});
+if (mix.inProduction()) {
+    mix.version();
+    mix.sourceMaps();
+}
